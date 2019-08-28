cf2a3febd7bcdbe0ebbda7c112417d97a8e667ee
Paul Kim <paulhyunkim@gmail.com>
2019-08-28 01:51:33 -0700

    Added view BlocksListViewModel and BlockDetailViewModel

---
+++ BlockOne/Scenes/BlockDetail/BlockDetailViewModel.swift
@@ -0,0 +1,19 @@
+//
+//  BlockDetailViewModel.swift
+//  BlockOne
+//
+//  Created by Paul Kim on 8/28/19.
+//  Copyright © 2019 Paul Kim. All rights reserved.
+//
+
+import Foundation
+
+struct BlockDetailViewModel {
+    
+    private let block: Block
+    
+    init(block: Block) {
+        self.block = block
+    }
+    
+}
---
+++ BlockOne/Scenes/BlocksList/BlocksListViewModel.swift
@@ -0,0 +1,19 @@
+//
+//  BlocksListViewModel.swift
+//  BlockOne
+//
+//  Created by Paul Kim on 8/28/19.
+//  Copyright © 2019 Paul Kim. All rights reserved.
+//
+
+import Foundation
+
+struct BlocksListViewModel {
+    
+    private let blocks: [Block]
+    
+    init(blocks: [Block]) {
+        self.blocks = blocks
+    }
+
+}
