00b0ac22b055d0a6a253e718739712dc50235583
Paul Kim <paulhyunkim@gmail.com>
2019-08-28 01:57:43 -0700

    Added BlockCell and its view model

---
+++ BlockOne/Scenes/BlocksList/BlockCell.swift
@@ -0,0 +1,24 @@
+//
+//  BlockCell.swift
+//  BlockOne
+//
+//  Created by Paul Kim on 8/28/19.
+//  Copyright © 2019 Paul Kim. All rights reserved.
+//
+
+import UIKit
+
+class BlockCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ BlockOne/Scenes/BlocksList/BlockCellViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  BlockCellViewModel.swift
+//  BlockOne
+//
+//  Created by Paul Kim on 8/28/19.
+//  Copyright © 2019 Paul Kim. All rights reserved.
+//
+
+import Foundation
+
+struct BlockCellViewModel {
+    
+    
+}
