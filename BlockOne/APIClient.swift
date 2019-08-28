d8cd48ffe78931bfe60fc31bf522fbc282f22f53
Paul Kim <paulhyunkim@gmail.com>
2019-08-28 00:47:30 -0700

    Implemented fetch blockchain info and fetch block (using shared url session for now)

--- BlockOne/APIClient.swift
+++ BlockOne/APIClient.swift
@@ -17,11 +17,55 @@ class APIClient: APIClientProtocol {
     }
     
     func fetchBlockchainInfo(_ completion: @escaping (APIResponse<BlockchainInfo>) -> Void) {
+        let url = URL(string: "https://api.eosnewyork.io/v1/chain/get_info")!
+        var urlRequest = URLRequest(url: url)
+        urlRequest.httpMethod = "POST"
         
+        sendRequest(urlRequest, completion: completion)
     }
     
     func fetchBlock(id: String, _ completion: @escaping (APIResponse<Block>) -> Void) {
+        let url = URL(string: "https://api.eosnewyork.io/v1/chain/get_block")!
+        var urlRequest = URLRequest(url: url)
+        urlRequest.httpMethod = "POST"
         
+        let parameterDictionary = ["block_num_or_id": id]
+        do {
+            let httpBody = try JSONSerialization.data(withJSONObject: parameterDictionary, options: [])
+            urlRequest.httpBody = httpBody
+        } catch let error {
+            return completion(.failure(error: error))
+        }
+        
+        sendRequest(urlRequest, completion: completion)
+    }
+    
+    private func sendRequest<T: Decodable>(_ urlRequest: URLRequest, completion: @escaping (APIResponse<T>) -> Void) {
+        let dataTask = urlSession.dataTask(with: urlRequest) { data, response, error in
+            guard let data = data else {
+                let error = NSError(domain: "No data was received in data task", code: 0, userInfo: nil)
+                completion(.failure(error: error))
+                return
+            }
+            do {
+                let decodedObject: T = try self.decodeData(data)
+                completion(.success(result: decodedObject))
+            } catch let error {
+                completion(.failure(error: error))
+            }
+            
+        }
+        dataTask.resume()
+    }
+    
+    private func decodeData<T: Decodable>(_ data: Data) throws -> T {
+        let decoder = JSONDecoder()
+        do {
+            let result = try decoder.decode(T.self, from: data)
+            return result
+        } catch let error {
+            throw error
+        }
     }
     
 }
