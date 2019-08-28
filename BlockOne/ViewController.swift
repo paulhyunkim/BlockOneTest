99989b612c702029978154153b3b0590475c005a
Paul Kim <paulhyunkim@gmail.com>
2019-08-28 00:04:54 -0700

    Initial Commit

---
+++ BlockOne/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BlockOne
+//
+//  Created by Paul Kim on 8/28/19.
+//  Copyright © 2019 Paul Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
